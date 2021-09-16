442f2cf53fbe41e8ed34c31f4d3127e88782cb19
kwjjjjj <kwjjjjj@abrain.co.kr>
2021-09-16 16:10:37 +0900

    [added] 프로젝트 생성

---
+++ app/src/main/java/woojin/android/kotlin/project/youtube/MainActivity.kt
@@ -0,0 +1,11 @@
+package woojin.android.kotlin.project.youtube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
